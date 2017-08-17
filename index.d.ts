4445d25955bdc309b63d3a7220613b35fa73ac7b
Alexander Ivantsov <alexivantsov@ya.ru>
2017-08-17 14:18:12 +0200

    update typescript definition

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,8 @@
-// Type definitions for "redux-subscriber" 1.1.0 
-// Project: redux-subscriber
-// Definitions by: eliasparis <https://github.com/eliasparis>
-
 declare module 'redux-subscriber' {
+    export default function(store: any): () => void;
     
-    export const subscribers: Object;
-
-    export function subscribe(key : string, cb : (state: any) => void) : void;
-
-    export default function( store: any) : () => void;
+    export function subscribe(
+        key: string, 
+        cb: (state: any) => void
+    ): () => void;
 }
