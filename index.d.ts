afa9f8e90a2f67b2a3f731e4fd677f575b0377f6
Alexander Ivantsov <alexivantsov@ya.ru>
2018-06-24 13:02:23 +0200

    move sources to src/

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'redux-subscriber' {
-    export default function(store: any): () => void;
-    
-    export function subscribe(
-        key: string, 
-        cb: (state: any) => void
-    ): () => void;
-}
---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'redux-subscriber' {
+  export default function(store: any): () => void;
+
+  export function subscribe(key: string, cb: (state: any) => void): () => void;
+}
